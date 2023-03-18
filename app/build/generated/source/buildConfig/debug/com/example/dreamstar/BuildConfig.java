f283e77db7a0ca0d773301aa0c6e8d8d06a35b2a
Harshana Chathuranga <91200650+wmhchathuranga@users.noreply.github.com>
2023-03-18 17:47:02 +0530

    First Push to main

---
+++ app/build/generated/source/buildConfig/debug/com/example/dreamstar/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.dreamstar;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.dreamstar";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
